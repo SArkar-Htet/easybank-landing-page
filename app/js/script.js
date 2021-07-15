da45a5ed88749fe5185fa547f2ad22c5ce5a5f89
SArkarHtet <developer.soearkarhtet@gmail.com>
2021-07-15 20:52:05 +0630

    updated: created animation for hamuburger menu using image

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+const btnHamburger = document.querySelector('#btnHamburger');
+const img = document.querySelector('#btnHamburger span img');
+btnHamburger.addEventListener('click', function() {
+  if (!btnHamburger.classList.contains('open')) {
+    btnHamburger.classList.add('open');
+    img.setAttribute('src', '/images/icon-close.svg');
+  } else {
+    btnHamburger.classList.remove('open');
+    img.setAttribute('src', '/images/icon-hamburger.svg');
+  }
+});

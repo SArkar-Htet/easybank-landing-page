586009fa6c13b5e7bf58d1cf7da610069afd1727
SArkarHtet <developer.soearkarhtet@gmail.com>
2021-07-16 10:16:18 +0630

    added anmiation in overlay for mobile view

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,13 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const overlay = document.querySelector('.overlay');
 btnHamburger.addEventListener('click', function() {
-  if (!btnHamburger.classList.contains('open')) {
+  if (!btnHamburger.classList.contains('open')) { 
     btnHamburger.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
   } else {
     btnHamburger.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
   }
 });

ad9f7b3ad7b01b4e7b86f855fb846712784a205c
SArkarHtet <developer.soearkarhtet@gmail.com>
2021-07-16 09:26:58 +0630

    created overlay for mobile view

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,8 @@
 const btnHamburger = document.querySelector('#btnHamburger');
-const img = document.querySelector('#btnHamburger span img');
 btnHamburger.addEventListener('click', function() {
   if (!btnHamburger.classList.contains('open')) {
     btnHamburger.classList.add('open');
-    img.setAttribute('src', '/images/icon-close.svg');
   } else {
     btnHamburger.classList.remove('open');
-    img.setAttribute('src', '/images/icon-hamburger.svg');
   }
 });

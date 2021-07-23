53baf992447b5e5606699b823f225b4e73094027
S.ArkarHtet <developer.soearkarhtet@gmail.com>
2021-07-23 16:08:31 +0630

    feat: add animation in header menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,18 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const overlay = document.querySelector('.overlay');
+const menu = document.querySelector('.header__menu');
 btnHamburger.addEventListener('click', function() {
   if (!btnHamburger.classList.contains('open')) { 
     btnHamburger.classList.add('open');
     overlay.classList.remove('fade-out');
     overlay.classList.add('fade-in');
+    menu.classList.remove('fade-out');
+    menu.classList.add('fade-in');
   } else {
     btnHamburger.classList.remove('open');
     overlay.classList.remove('fade-in');
     overlay.classList.add('fade-out');
+    menu.classList.remove('fade-in');
+    menu.classList.add('fade-out');
   }
 });

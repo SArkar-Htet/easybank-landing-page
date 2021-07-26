e8a6763399a9674911ceca8bfd9e5ec7318ec7bd
S.ArkarHtet <developer.soearkarhtet@gmail.com>
2021-07-26 11:17:35 +0630

    feat: add no scroll feature

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,16 @@
+const noScroll = document.querySelector('body');
 const btnHamburger = document.querySelector('#btnHamburger');
 const fadeElems = document.querySelectorAll('.has_fade');
 btnHamburger.addEventListener('click', function() {
   if (!btnHamburger.classList.contains('open')) { //Open Hamburger Menu
+    noScroll.classList.add('no_scroll');
     btnHamburger.classList.add('open');
     fadeElems.forEach(function (element) {
       element.classList.remove('fade-out');
       element.classList.add('fade-in');
     });
   } else { //Close Hamburger Menu
+    noScroll.classList.remove('no_scroll');
     btnHamburger.classList.remove('open');
     fadeElems.forEach(function (element) {
       element.classList.remove('fade-in');

9f17aafd7b1c1788092e42a388bc69f1051d4f61
Soe Arkar Htet <developer.soearkarhtet@gmail.com>
2021-11-14 15:54:07 +0630

    fix: modify hamburger menu to improve accessible

--- app/js/script.js
+++ app/js/script.js
@@ -5,6 +5,7 @@ btnHamburger.addEventListener('click', function() {
   if (!btnHamburger.classList.contains('open')) { //Open Hamburger Menu
     noScroll.classList.add('no_scroll');
     btnHamburger.classList.add('open');
+    btnHamburger.setAttribute("aria-expanded", "true");
     fadeElems.forEach(function (element) {
       element.classList.remove('fade-out');
       element.classList.add('fade-in');
@@ -12,6 +13,7 @@ btnHamburger.addEventListener('click', function() {
   } else { //Close Hamburger Menu
     noScroll.classList.remove('no_scroll');
     btnHamburger.classList.remove('open');
+    btnHamburger.setAttribute("aria-expanded", "false");
     fadeElems.forEach(function (element) {
       element.classList.remove('fade-in');
       element.classList.add('fade-out');
